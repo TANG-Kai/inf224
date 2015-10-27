66d78efd75004e453d84dcd7f6bb2dbd9869b29e
TANG Kai <kai.tang@telecom-paristech.fr>
2015-10-27 11:13:27 +0100

    5e done.Answering question: The property permetting this feature is heritage and polymorphism that comes with heritage. It's especially neccessary in c++ if we want to make our objects expansible, or if we didn't write the virtuel function in basic class, the program wouldn't compile. We can no longer define an object of the basic class since there are virtuel functions in it and virtuel functions demand implementing.

--- main.cpp
+++ main.cpp
@@ -4,13 +4,22 @@
 int main()
 {
     Media_video* mv = new Media_video("obj1", "drop.avi",2);
-    Media_base* mv_base = mv;
-    mv_base->show_values(std::cout);
-    mv_base->play();
+//    Media_base* mv_base = mv;
+//    mv_base->show_values(std::cout);
+//    mv_base->play();
 
     Media_photo * mp = new Media_photo("obj2", "lena_color.gif");
-    mv_base = mp;
-    mv_base->play();
+//    mv_base = mp;
+//    mv_base->play();
+
+    Media_base**  mb;
+    mb = new Media_base* [2];
+    mb[0] = mv;
+    mb[1] = mp;
+    for(int i=0;i<2; i++){
+      mb[i]->play();
+    }
+
 
     return 0;
 }
